5d9b9c991d5a33ea0ef5c2d5165fed855c7a4696
Joe Bernardi <joebernardi@hotmail.com>
2016-06-23 12:26:42 -0500

    set up files

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,18 @@
 # Your code here
+require 'calculator'
+
+describe Calculator do
+
+	describe '#initialize' do
+
+		it 'raises and ArgumentError with 2+ arguements' do
+
+			expect{ Calculator.new(5,5) }.to raise_error( ArgumentError )
+
+		end
+
+
+	end
+
+
+end
